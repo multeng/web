698317cbeadd3a228cf4c3d0663e4b8a66434e88
Vladimir Kormakov <cenizo.kht@gmail.com>
2019-11-27 15:46:41 +0300

    added some dirs

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,17 +13,17 @@ class QuestionManager(models.Manager):
 		return self.order_by('-likes')
 
 class Question(models.Model):
-	object = QuestionManager()
+	objects = QuestionManager()
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeFiled(auto_now_add=True)
+	added_at = models.DateTimeField(auto_now_add=True)
 	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User, related_name="question_author", on_delete=models.SET_DEFAULT, default="",null=True)
 	likes = models.ManyToManyField(User, related_name="question_like_user")
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeFiled(auto_now_add=True)
+	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question, default="", on_delete=models.CASCADE)
 	author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="",null=True)
 	
