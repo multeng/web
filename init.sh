1bbb506a97ec826ffb0e88c61f1a636c82a2b098
Vladimir Kormakov <cenizo.kht@gmail.com>
2019-11-27 15:24:00 +0300

    added some dirs

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 rm /etc/nginx/sites-enabled/default
 cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo apt update
+sudo pip install gunicorn
+sudo install django==1.6.1
 /etc/init.d/nginx restart
